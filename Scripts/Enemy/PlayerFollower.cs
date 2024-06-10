c7632f2a21e402d88a5d1d9e3a5b8bc012b6017e
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2024-06-10 21:18:16 +0500

    Update PlayerFollower.cs

--- Scripts/Enemy/PlayerFollower.cs
+++ Scripts/Enemy/PlayerFollower.cs
@@ -7,14 +7,4 @@ public class PlayerFollower : MonoBehaviour
     [SerializeField] private EnemyMover _patrol;
     [SerializeField] private float _speed;
     [SerializeField] private Player _target;
-
-    private void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-    }
 }
